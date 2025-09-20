8148db880a4ae7c4fa1eb365ad27fc1cbd00aa1a
meheraj786 <meherajhosen786@gmail.com>
2025-09-20 21:51:45 +0600

    input components more props recive

--- src/layout/Input.jsx
+++ src/layout/Input.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
 
-const Input = ({type,placeholder}) => {
+const Input = ({ type, placeholder, value, onChange }) => {
   return (
-    <input type={type || "text"} placeholder={placeholder} className={`w-full rounded-[8px] text-left px-4 py-4 text-[18px] bg-[#F1F4F9] border border-[#D8D8D8] text-[#A6A6A6]`} />
-  )
-}
+    <input
+      value={value}
+      onChange={onChange}
+      type={type || "text"}
+      placeholder={placeholder}
+      className={`w-full rounded-[8px] text-left px-4 py-4 text-[18px] bg-[#F1F4F9] border border-[#D8D8D8] text-[#A6A6A6]`}
+    />
+  );
+};
 
-export default Input
+export default Input;
