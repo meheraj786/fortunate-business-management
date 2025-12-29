11f0a71892b850359b2f8da007b71997956125a9
meheraj786 <meherajhosen786@gmail.com>
2025-12-29 09:05:40 +0600

    deletion and bank detail update for trash management

--- src/components/common/TransactionTable.jsx
+++ src/components/common/TransactionTable.jsx
@@ -100,7 +100,7 @@ const TransactionTable = ({ transactions, onRowClick }) => {
                   </span>
                 </td>
                 <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-700">
-                  {getAccountDisplayName(transaction.accountId)}
+                  {getAccountDisplayName(transaction.account)}
                 </td>
                 <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
                   {new Date(transaction.date).toLocaleDateString("en-GB")}
